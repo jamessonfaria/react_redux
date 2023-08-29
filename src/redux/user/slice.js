9584ea137350778b9c2c4f6371e4dbabeb915850
Jamesson Faria <jamessonjr@gmail.com>
2023-08-29 14:53:29 -0300

    added redux to projeect

---
+++ src/redux/user/slice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    createUser: (state, action) => {
+      return {
+        ...state,
+        user: {
+          name: action.payload.name,
+          email: action.payload.email,
+          address: {
+            location: "Rua paraiba",
+            number: 34,
+          },
+        },
+      };
+    },
+  },
+});
+
+export const { createUser } = userSlice.actions;
+
+export default userSlice.reducer;
